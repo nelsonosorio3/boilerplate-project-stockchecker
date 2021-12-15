6b88615cbe9eb8c382f4d1bf0191ed008327662e
Nelson Alejandro Osorio Ramirez <nelsonsorio@hotmail.com>
2021-12-15 18:16:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
 
+const helmet = require("helmet");
+
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -16,6 +18,13 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({action: "deny"}));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+app.use(helmet.contentSecurityPolicy({directives: {defaultSrc: ["'self'"], scriptSrc: ["'self'"], "style-src": ["'self'"]}}))
 
 //Index page (static HTML)
 app.route('/')
